40e0de6ca77ec7e84147cb6050ecd2307eec4d15
fahmiidris <fahmiidris@apps.ipb.ac.id>
2024-04-17 16:27:14 +0700

    feat(repositories): add repository user in domain layer

---
+++ lib/domain/repositories/user_repository.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:mobile_user_accurate/common/failure.dart';
+import 'package:mobile_user_accurate/domain/entities/user.dart';
+
+abstract class UserRepository {
+  Future<Either<Failure, List<User>>> getUsers();
+  Future<Either<Failure, List<User>>> addUser(User user);
+}
