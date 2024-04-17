12454ba9651618db7a183e0be1c29ab3f609e342
fahmiidris <fahmiidris@apps.ipb.ac.id>
2024-04-17 16:26:12 +0700

    feat(entities): add entities user in domain layer

---
+++ lib/domain/entities/user.dart
@@ -0,0 +1,21 @@
+import 'package:equatable/equatable.dart';
+
+class User extends Equatable {
+  final String id;
+  final String name;
+  final String email;
+  final String address;
+  final String phoneNumber;
+  final String city;
+
+  const User(
+      {required this.id,
+      required this.name,
+      required this.email,
+      required this.address,
+      required this.phoneNumber,
+      required this.city});
+
+  @override
+  List<Object> get props => [id, name, email, address, phoneNumber, city];
+}
